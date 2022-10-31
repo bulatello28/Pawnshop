44dbccff3504439ff2bb50fd54ce7f4f04ab66fe
bulatello28 <bulatalmazovich.99@mail.ru>
2022-10-31 14:59:08 +0300

    Добавьте файлы проекта.

---
+++ Pawnshop/ClassApp/DbConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pawnshop.AdoApp;
+
+namespace Pawnshop.ClassApp
+{
+    public class DbConnection
+    {
+        public static LombardEntities Connection = new LombardEntities();
+    }
+}
---
+++ Pawnshop/Pages/Products.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Pawnshop.ClassApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pawnshop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Products.xaml
+    /// </summary>
+    public partial class Products : Page
+    {
+        public Products()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductsListBox_Initialized(object sender, EventArgs e)
+        {
+            ProductsListBox.ItemsSource = DbConnection.Connection.Product.ToList();
+        }
+    }
+}
